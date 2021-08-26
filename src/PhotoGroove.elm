8e6c0954d14f5ddd239134c47231ce323e553cbb
David Senack <david.senack@gmail.com>
2021-08-26 15:39:06 -0400

    updated button and style

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -12,9 +12,13 @@ urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { description = "ClickedSurpriseMe", data = "" } ]
+            [ text "Surprise Me!" ]
         , div [ id "thumbnails"] 
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
@@ -24,6 +28,7 @@ view model =
             []
         ]
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumb =
     img [ src (urlPrefix ++ thumb.url)
     , classList [ ( "selected", selectedUrl == thumb.url ) ]
@@ -31,7 +36,17 @@ viewThumbnail selectedUrl thumb =
     ]
     [] 
 
-initialModel : { photos : List { url : String }, selectedUrl : String }
+type alias Photo =
+    { url : String }
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+type alias Msg =
+    { description : String, data : String }
+
+initialModel : Model
 initialModel = 
     { photos = 
         [ { url = "1.jpeg" }
@@ -41,15 +56,22 @@ initialModel =
     , selectedUrl = "1.jpeg"
     }
 
-photoArray : Array { url : String }
+photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
 
+update : Msg -> Model -> Model
 update msg model = 
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
-    else
-        model
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
+        
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _->
+            model
+
 
 main =
     Browser.sandbox
